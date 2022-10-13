b46feadb5a31ca7e5bb13a8eac70a66dac9ad6bb
Rasmus Jensen <rasmusdj96@gmail.com>
2022-10-13 14:03:27 +0200

    123

---
+++ assets/js/productItems.js
@@ -0,0 +1,256 @@
+let productItems = [
+	{
+		id: 0,
+		title: "iPhone 13 Pro 128GB",
+		brand: "Apple",
+		category: "Smartphones",
+		price: 8000,
+		thumbnail:
+			"https://www.pricerunner.dk/product/640x640/3002699023/Apple-iPhone-13-Pro-128GB.jpg",
+		images: [
+			"https://www.pricerunner.dk/product/640x640/3002699430/Apple-iPhone-13-Pro-128GB.jpg",
+			"https://www.pricerunner.dk/product/640x640/3002699431/Apple-iPhone-13-Pro-128GB.jpg",
+			"https://www.pricerunner.dk/product/640x640/3002699432/Apple-iPhone-13-Pro-128GB.jpg",
+		],
+	},
+	{
+		id: 1,
+		title: "iPhone 13 128GB",
+		brand: "Apple",
+		category: "Smartphones",
+		price: 6500,
+		thumbnail:
+			"https://www.pricerunner.dk/product/640x640/3002699020/Apple-iPhone-13-128GB.jpg",
+		images: [
+			"https://www.pricerunner.dk/product/640x640/3002700176/Apple-iPhone-13-128GB.jpg",
+			"https://www.pricerunner.dk/product/640x640/3002700177/Apple-iPhone-13-128GB.jpg",
+			"https://www.pricerunner.dk/product/640x640/3002700177/Apple-iPhone-13-128GB.jpg",
+		],
+	},
+	{
+		id: 2,
+		title: "iPhone SE 64GB",
+		brand: "Apple",
+		category: "Smartphones",
+		price: 3000,
+		thumbnail:
+			"https://www.pricerunner.dk/product/640x640/3004077643/Apple-iPhone-SE-64GB-(3rd-Generation).jpg",
+		images: [
+			"https://www.pricerunner.dk/product/640x640/3004078061/Apple-iPhone-SE-64GB-(3rd-Generation).jpg",
+			"https://www.pricerunner.dk/product/640x640/3004080237/Apple-iPhone-SE-64GB-(3rd-Generation).jpg",
+			"https://www.pricerunner.dk/product/640x640/3004080238/Apple-iPhone-SE-64GB-(3rd-Generation).jpg",
+		],
+	},
+	{
+		id: 3,
+		title: "iPhone 14 Pro 128GB",
+		brand: "Apple",
+		category: "Smartphones",
+		price: 11000,
+		thumbnail:
+			"https://www.pricerunner.dk/product/640x640/3006260317/Apple-iPhone-14-Pro-128GB.jpg",
+		images: [
+			"https://www.pricerunner.dk/product/640x640/3006232243/Apple-iPhone-14-Pro-128GB.jpg",
+			"https://www.pricerunner.dk/product/640x640/3006232244/Apple-iPhone-14-Pro-128GB.jpg",
+			"https://www.pricerunner.dk/product/640x640/3006232245/Apple-iPhone-14-Pro-128GB.jpg",
+		],
+	},
+	{
+		id: 4,
+		title: "iPhone 11 64GB",
+		brand: "Apple",
+		category: "Smartphones",
+		price: 3500,
+		thumbnail:
+			"https://www.pricerunner.dk/product/640x640/1906996022/Apple-iPhone-11-64GB.jpg",
+		images: [
+			"https://www.pricerunner.dk/product/640x640/3002387430/Apple-iPhone-11-64GB.jpg",
+			"https://www.pricerunner.dk/product/640x640/3004806772/Apple-iPhone-11-64GB.jpg",
+			"https://www.pricerunner.dk/product/640x640/3002387431/Apple-iPhone-11-64GB.jpg",
+		],
+	},
+	{
+		id: 5,
+		title: "iPhone 14 512GB",
+		brand: "Apple",
+		category: "Smartphones",
+		price: 15000,
+		thumbnail:
+			"https://www.pricerunner.dk/product/640x640/3006260337/Apple-iPhone-14-Pro-512GB.jpg",
+		images: [
+			"https://www.pricerunner.dk/product/640x640/3006232124/Apple-iPhone-14-Pro-512GB.jpg",
+			"https://www.pricerunner.dk/product/640x640/3006232125/Apple-iPhone-14-Pro-512GB.jpg",
+			"https://www.pricerunner.dk/product/640x640/3006232126/Apple-iPhone-14-Pro-512GB.jpg",
+		],
+	},
+	{
+		id: 6,
+		title: "iPhone SE 128GB",
+		brand: "Apple",
+		category: "Smartphones",
+		price: 3000,
+		thumbnail:
+			"https://www.pricerunner.dk/product/640x640/3000280849/Apple-iPhone-SE-128GB-(2nd-Generation).jpg",
+		images: [
+			"https://www.pricerunner.dk/product/640x640/3000280959/Apple-iPhone-SE-128GB-(2nd-Generation).jpg",
+			"https://www.pricerunner.dk/product/640x640/3000280958/Apple-iPhone-SE-128GB-(2nd-Generation).jpg",
+			"https://www.pricerunner.dk/product/640x640/3000280938/Apple-iPhone-SE-128GB-(2nd-Generation).jpg",
+		],
+	},
+	{
+		id: 7,
+		title: "Samsung Galaxy S22 128GB",
+		brand: "Samsung",
+		category: "Smartphones",
+		price: 5000,
+		thumbnail:
+			"https://www.pricerunner.dk/product/640x640/3003944727/Samsung-Galaxy-S22-128GB.jpg",
+		images: [
+			"https://www.pricerunner.dk/product/640x640/3003941784/Samsung-Galaxy-S22-128GB.jpg",
+			"https://www.pricerunner.dk/product/640x640/3003941785/Samsung-Galaxy-S22-128GB.jpg",
+			"https://www.pricerunner.dk/product/640x640/3003941786/Samsung-Galaxy-S22-128GB.jpg",
+		],
+	},
+	{
+		id: 8,
+		title: "Samsung Galaxy S22 Ultra 128GB",
+		brand: "Samsung",
+		category: "Smartphones",
+		price: 7800,
+		thumbnail:
+			"https://www.pricerunner.dk/product/640x640/3003944107/Samsung-Galaxy-S22-Ultra-128GB.jpg",
+		images: [
+			"https://www.pricerunner.dk/product/640x640/3003944108/Samsung-Galaxy-S22-Ultra-128GB.jpg",
+			"https://www.pricerunner.dk/product/640x640/3003944109/Samsung-Galaxy-S22-Ultra-128GB.jpg",
+			"https://www.pricerunner.dk/product/640x640/3003944110/Samsung-Galaxy-S22-Ultra-128GB.jpg",
+		],
+	},
+	{
+		id: 9,
+		title: "Samsung Galaxy Z Fold4 256GB",
+		brand: "Samsung",
+		category: "Smartphones",
+		price: 11000,
+		thumbnail:
+			"https://www.pricerunner.dk/product/640x640/3005888357/Samsung-Galaxy-Z-Fold4-256GB.jpg",
+		images: [
+			"https://www.pricerunner.dk/product/640x640/3005888358/Samsung-Galaxy-Z-Fold4-256GB.jpg",
+			"https://www.pricerunner.dk/product/640x640/3005888359/Samsung-Galaxy-Z-Fold4-256GB.jpg",
+			"https://www.pricerunner.dk/product/640x640/3005888360/Samsung-Galaxy-Z-Fold4-256GB.jpg",
+		],
+	},
+	{
+		id: 10,
+		title: "Samsung Galaxy Z Flip4 128GB",
+		brand: "Samsung",
+		category: "Smartphones",
+		price: 5500,
+		thumbnail:
+			"https://www.pricerunner.dk/product/640x640/3005888210/Samsung-Galaxy-Z-Flip4-128GB.jpg",
+		images: [
+			"https://www.pricerunner.dk/product/640x640/3005888211/Samsung-Galaxy-Z-Flip4-128GB.jpg",
+			"https://www.pricerunner.dk/product/640x640/3005888212/Samsung-Galaxy-Z-Flip4-128GB.jpg",
+			"https://www.pricerunner.dk/product/640x640/3005888213/Samsung-Galaxy-Z-Flip4-128GB.jpg",
+		],
+	},
+	{
+		id: 11,
+		title: "MacBook Air (2022)",
+		brand: "Apple",
+		category: "Laptops",
+		price: 11000,
+		thumbnail:
+			"https://www.pricerunner.dk/product/640x640/3004892914/Apple-MacBook-Air-(2022)-M2-OC-8C-GPU-8GB-256GB-SSD-13.6-.jpg",
+		images: [
+			"https://www.pricerunner.dk/product/640x640/3004892915/Apple-MacBook-Air-(2022)-M2-OC-8C-GPU-8GB-256GB-SSD-13.6-.jpg",
+			"https://www.pricerunner.dk/product/640x640/3005893800/Apple-MacBook-Air-(2022)-M2-OC-8C-GPU-8GB-256GB-SSD-13.6-.jpg",
+			"https://www.pricerunner.dk/product/640x640/3005893801/Apple-MacBook-Air-(2022)-M2-OC-8C-GPU-8GB-256GB-SSD-13.6-.jpg",
+		],
+	},
+	{
+		id: 12,
+		title: "GF63 Thin",
+		brand: "MSI",
+		category: "Laptops",
+		price: 4500,
+		thumbnail:
+			"https://www.pricerunner.dk/product/640x640/3004927887/MSI-GF63-Thin-11SC-687NEU.jpg",
+		images: [
+			"https://www.pricerunner.dk/product/640x640/3004927888/MSI-GF63-Thin-11SC-687NEU.jpg",
+			"https://www.pricerunner.dk/product/640x640/3004927889/MSI-GF63-Thin-11SC-687NEU.jpg",
+			"https://www.pricerunner.dk/product/640x640/3004927890/MSI-GF63-Thin-11SC-687NEU.jpg",
+		],
+	},
+	{
+		id: 13,
+		title: "IdeaPad Flex 5",
+		brand: "Lenovo",
+		category: "Laptops",
+		price: 8400,
+		thumbnail:
+			"https://www.pricerunner.dk/product/640x640/3000921496/Lenovo-IdeaPad-Flex-5-82HS003QGE.jpg",
+		images: [
+			"https://www.pricerunner.dk/product/640x640/3000921498/Lenovo-IdeaPad-Flex-5-82HS003QGE.jpg",
+			"https://www.pricerunner.dk/product/640x640/3000921501/Lenovo-IdeaPad-Flex-5-82HS003QGE.jpg",
+			"https://www.pricerunner.dk/product/640x640/3000921512/Lenovo-IdeaPad-Flex-5-82HS003QGE.jpg",
+		],
+	},
+	{
+		id: 14,
+		title: "Zenbook Pro Duo 14 OLED",
+		brand: "ASUS",
+		category: "Laptops",
+		price: 23000,
+		thumbnail:
+			"https://www.pricerunner.dk/product/640x640/3005144847/ASUS-Zenbook-Pro-Duo-14-OLED-UX8402ZE-M3100X.jpg",
+		images: [
+			"https://www.pricerunner.dk/product/640x640/3005144848/ASUS-Zenbook-Pro-Duo-14-OLED-UX8402ZE-M3100X.jpg",
+			"https://www.pricerunner.dk/product/640x640/3005144849/ASUS-Zenbook-Pro-Duo-14-OLED-UX8402ZE-M3100X.jpg",
+			"https://www.pricerunner.dk/product/640x640/3005144850/ASUS-Zenbook-Pro-Duo-14-OLED-UX8402ZE-M3100X.jpg",
+		],
+	},
+	{
+		id: 15,
+		title: "MacBook Pro (2021)",
+		brand: "Apple",
+		category: "Laptops",
+		price: 19600,
+		thumbnail:
+			"https://www.pricerunner.dk/product/640x640/3002952128/Apple-MacBook-Pro-(2021)-M1-Pro-10C-CPU-16C-GPU-16GB-512GB-SSD-16-.jpg",
+		images: [
+			"https://www.pricerunner.dk/product/640x640/3006230309/Apple-MacBook-Pro-(2021)-M1-Pro-10C-CPU-16C-GPU-16GB-512GB-SSD-16-.jpg",
+			"https://www.pricerunner.dk/product/640x640/3006261050/Apple-MacBook-Pro-(2021)-M1-Pro-10C-CPU-16C-GPU-16GB-512GB-SSD-16-.jpg",
+			"https://www.pricerunner.dk/product/640x640/3006261051/Apple-MacBook-Pro-(2021)-M1-Pro-10C-CPU-16C-GPU-16GB-512GB-SSD-16-.jpg",
+		],
+	},
+	{
+		id: 16,
+		title: "Raider GE67HX",
+		brand: "MSI",
+		category: "Laptops",
+		price: 23000,
+		thumbnail:
+			"https://www.pricerunner.dk/product/640x640/3005116792/MSI-Raider-GE67HX-12UGS-012NEU.jpg",
+		images: [
+			"https://www.pricerunner.dk/product/640x640/3005116793/MSI-Raider-GE67HX-12UGS-012NEU.jpg",
+			"https://www.pricerunner.dk/product/640x640/3005116794/MSI-Raider-GE67HX-12UGS-012NEU.jpg",
+			"https://www.pricerunner.dk/product/640x640/3005116795/MSI-Raider-GE67HX-12UGS-012NEU.jpg",
+		],
+	},
+	{
+		id: 17,
+		title: "IdeaPad Gaming 3",
+		brand: "Lenovo",
+		category: "Laptops",
+		price: 23000,
+		thumbnail:
+			"https://www.pricerunner.dk/product/640x640/3004483683/Lenovo-IdeaPad-Gaming-3-15ACH6-82K200N1MX.jpg",
+		images: [
+			"https://www.pricerunner.dk/product/640x640/3004483684/Lenovo-IdeaPad-Gaming-3-15ACH6-82K200N1MX.jpg",
+			"https://www.pricerunner.dk/product/640x640/3004483685/Lenovo-IdeaPad-Gaming-3-15ACH6-82K200N1MX.jpg",
+			"https://www.pricerunner.dk/product/640x640/3004483686/Lenovo-IdeaPad-Gaming-3-15ACH6-82K200N1MX.jpg",
+		],
+	},
+];
+
+export default productItems;
---
+++ assets/js/products.js
@@ -0,0 +1,29 @@
+import productItems from "./productItems.js";
+
+let productContainer = document.getElementById("products");
+
+let renderAllContent = () => {
+	productItems.forEach((product) => {
+		console.log(product);
+		if (!document.getElementById(`${product.category}`)) {
+			let categoryname = document.createElement("h2");
+			productContainer.appendChild(categoryname);
+			categoryname.innerHTML = `${product.category}`;
+			let category = document.createElement("div");
+			productContainer.appendChild(category);
+			category.setAttribute("id", `${product.category}`);
+			category.classList.add("productFlex");
+		}
+		document.getElementById(`${product.category}`).innerHTML += `
+        <div class="cardDesign">
+        <h4>${product.brand}</h4>
+        <h3>${product.title}</h3>
+        <div class="imgContainer"><img src="${product.thumbnail}"></div>
+        <p>${product.price} DKK</p>
+        <button>Læg i kurv</button>
+        </div>
+        `;
+	});
+};
+
+renderAllContent();
