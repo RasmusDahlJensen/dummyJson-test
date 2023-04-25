31316b7c39fc0ef6db9024816d0fe2baec549d75
Rasmus Jensen <rasmusdj96@gmail.com>
2023-04-25 10:11:22 +0200

    Added Dynamic caching, cache limiting and cleaning old caching

--- sw.js
+++ sw.js
@@ -9,7 +9,6 @@ const assets = [
 	"/manifest.json",
 ];
 
-//Åben og limit cache
 const limitCacheSize = (cacheName, numAllowedFiles) => {
 	caches.open(cacheName).then((cache) => {
 		cache.keys().then((keys) => {
@@ -20,17 +19,21 @@ const limitCacheSize = (cacheName, numAllowedFiles) => {
 	});
 };
 
-// Install Service Worker
+//Install service worker
 self.addEventListener("install", (event) => {
+	// console.log("Service Worker has been installed", event);
+
 	event.waitUntil(
 		caches.open(staticCacheName).then((cache) => {
+			// console.log("Caching all assets", cache);
 			cache.addAll(assets);
 		})
 	);
 });
 
-// Install Service Worker
+//Activate serviceworker
 self.addEventListener("activate", (event) => {
+	// console.log("Service worker has been activated", event);
 	event.waitUntil(
 		caches.keys().then((keys) => {
 			return Promise.all(
@@ -42,7 +45,23 @@ self.addEventListener("activate", (event) => {
 	);
 });
 
-// Fetch event
+// Fetch
 self.addEventListener("fetch", (event) => {
-	// Kontroller svar på request
+	if (!(event.request.url.indexOf("http") === 0)) return;
+	// console.log(event.request);
+
+	event.respondWith(
+		caches.match(event.request).then((cacheRes) => {
+			return (
+				cacheRes ||
+				fetch(event.request).then((fetchRes) => {
+					return caches.open(dynamicCacheName).then((cache) => {
+						cache.put(event.request.url, fetchRes.clone());
+						limitCacheSize(dynamicCacheName, 2);
+						return fetchRes;
+					});
+				})
+			);
+		})
+	);
 });
